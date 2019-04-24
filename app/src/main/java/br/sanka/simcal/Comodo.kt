71b0abd72c511ab918755c237f6eabc4831ec97c
samuel da silva morais <sanka1375@gmail.com>
2019-04-24 20:28:48 -0300

    Logica para Troca de Mensagens entre Dispositivos

--- app/src/main/java/br/sanka/simcal/Comodo.kt
+++ app/src/main/java/br/sanka/simcal/Comodo.kt
@@ -1,6 +1,6 @@
 package br.sanka.simcal
 
 class Comodo constructor(nome : String){
-
+    var dispositivos_conectados : ArrayList<Dispositivo> = arrayListOf()
 
 }
--- app/src/main/java/br/sanka/simcal/Mensagem.kt
+++ app/src/main/java/br/sanka/simcal/Mensagem.kt
@@ -2,5 +2,21 @@ package br.sanka.simcal
 
 import java.util.*
 
-class Mensagem constructor(data : Date, texto: String, origem : Dispositivo, tipo : String) {
+class Mensagem constructor(data : Date, texto: String, origem : Dispositivo, tipo : Int) {
+    var data : Date = data
+    var texto: String = texto
+    var origem : Dispositivo = origem
+    var tipo : Int = tipo
+
+    companion object {
+        const val   Log : Int = 0
+        const val   Acao : Int = 1
+        const val   Error : Int = 2
+        const val   Aviso : Int = 3
+        const val   Status : Int = 4
+        const val   Requisicao : Int = 5
+        const val   Resposta : Int = 6
+    }
+
+
 }
